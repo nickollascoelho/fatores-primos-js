db975badad5509314d0bd76c10a7f8a189b3ea05
Nickollas Coelho <nickollas.coelho@gmail.com>
2017-05-23 00:20:52 -0300

    Refatoracao para facilitar leitura

--- fatores-primos-nickollas.js
+++ fatores-primos-nickollas.js
@@ -5,8 +5,15 @@ const primeFactorsCache = {};
 function getPrimeFactors(number) {
   if (isNaN(number) || number < 2) return [];
   if (!Number.isInteger(number)) return [];
+
   if (primeFactorsCache.hasOwnProperty(number)) return primeFactorsCache[number];
-  const original = number;
+
+  const primeFactors = calcPrimeFactors(number);
+  primeFactorsCache[number] = primeFactors;
+  return primeFactors;
+}
+
+function calcPrimeFactors(number) {
   const primeFactors = [];
 
   for (let i = 2; i <= number / i; i++) {
@@ -17,8 +24,6 @@ function getPrimeFactors(number) {
   }
 
   if (number > 1) primeFactors.push(number);
-
-  primeFactorsCache[original] = primeFactors;
 
   return primeFactors;
 }
